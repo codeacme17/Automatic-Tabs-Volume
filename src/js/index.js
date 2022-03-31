67d415f5b7313bc388b3fcfd970b8a1ef1654a33
codeacme17 <vir_kings@qq.com>
2022-03-31 22:01:35 +0800

    @2022/3/31

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+chrome.tabs.onActivated.addListener((activeInfo) => move(activeInfo));
+
+function move(activeInfo) {
+  let allTabs = [];
+
+  chrome.tabs.query({}, function (tabs) {
+    allTabs = tabs;
+  });
+
+  chrome.tabs.get(activeInfo.tabId, (info) => {
+    toggleMuteState(info, allTabs);
+  });
+}
+
+function toggleMuteState(tab, allTabs) {
+  if (tab.audible) {
+    allTabs.forEach((singleTab) => {
+      if (singleTab.id === tab.id) {
+        chrome.tabs.update(tab.id, { muted: false });
+      } else {
+        chrome.tabs.update(singleTab.id, { muted: true });
+      }
+    });
+  } else {
+    chrome.tabs.update(tab.id, { muted: false });
+  }
+}
+
+chrome.tabs.getSelected(null, function (tab) {
+  chrome.tabs.sendMessage(tab.id, {}, function (response) {
+    console.log(response);
+  });
+});
